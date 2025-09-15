fa681e32f9e1f86c7d5284a1b61b1fe7e64c6b46
MJ_Akbari <mojav.akbari@gmail.com>
2025-09-16 00:43:25 +0330

    migrate

--- accounts/migrations/0003_merge.py
+++ accounts/migrations/0003_merge.py
@@ -4,7 +4,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('accounts', '0002_alter_customuser_phone_number'),
-        ('accounts', '0002_alter_customuser_profile_picture'),
     ]
 
     operations = []
