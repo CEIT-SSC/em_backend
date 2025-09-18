d18aed602cf75b0d1e8c7618e4ba0664625bb2f3
MJ_Akbari <mojav.akbari@gmail.com>
2025-09-18 16:25:12 +0330

    debug mode

--- em_backend/settings.py
+++ em_backend/settings.py
@@ -244,17 +244,17 @@ WSGI_APPLICATION = 'em_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } if DEBUG else {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+    # } if DEBUG else {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'default_db_name'),
         'USER': os.getenv('DB_USER', 'default_user'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'default_password'),
         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    # }
 }
 
 
