29ec744a5ae10e44b09de222114798e9f1c9cf64
mahanzavari <mahanzavari@gmail.com>
2026-08-11 00:30:18 +0330

    fix(shop/urls): removed payments urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import (
-    CartView, CartItemView, ApplyDiscountView, RemoveDiscountView,
-    OrderCheckoutView, OrderPaymentInitiateView, PaymentCallbackView,
-    OrderHistoryViewSet, OrderCancelView, UserPurchasesView, ProductListView, CartPaymentInitiateView,
-    TeamPaymentInitiateView
+    ApplyDiscountView,
+    CartItemView,
+    CartView,
+    OrderCancelView,
+    OrderCheckoutView,
+    OrderHistoryViewSet,
+    ProductListView,
+    RemoveDiscountView,
+    UserPurchasesView,
 )
 
 app_name = 'shop'
@@ -19,11 +24,7 @@ urlpatterns = [
     path('cart/remove-discount/', RemoveDiscountView.as_view(), name='cart-remove-discount'),
 
     path('orders/checkout/', OrderCheckoutView.as_view(), name='order-checkout'),
-    path('orders/<uuid:order_id>/initiate-payment/', OrderPaymentInitiateView.as_view(), name='order-initiate-payment'),
-    path('orders/initiate-from-cart/', CartPaymentInitiateView.as_view(), name='initiate-from-cart'),
-    path("orders/<uuid:order_id>/cancel/", OrderCancelView.as_view(), name="order-cancel-by-pk"),
-    path('teams/<int:team_id>/initiate-payment/', TeamPaymentInitiateView.as_view(), name='team-initiate-payment'),
-    path('payment/callback/', PaymentCallbackView.as_view(), name='payment_callback'),
+    path('orders/<uuid:order_id>/cancel/', OrderCancelView.as_view(), name='order-cancel-by-pk'),
 
     path('purchases/', UserPurchasesView.as_view(), name='user-purchases'),
     path('products/', ProductListView.as_view(), name='product-list'),
